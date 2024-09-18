da395f6c5630b7279e22751b3412037df5d52731
Armin <62218506+SagMeinenNamen@users.noreply.github.com>
2024-09-19 00:06:45 +0200

    Routing fix (currenlty with hash routing)

--- OpenPlace/src/app/app.component.ts
+++ OpenPlace/src/app/app.component.ts
@@ -1,12 +1,11 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [RouterOutlet, CommonModule, RouterLink],
+  imports: [RouterOutlet],
   standalone: true,
 })
 export class AppComponent {
