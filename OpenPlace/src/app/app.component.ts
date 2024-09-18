f71da8d64ff80f036e6c07901c0f31cf4bddbcb6
Armin <62218506+SagMeinenNamen@users.noreply.github.com>
2024-09-19 00:17:55 +0200

    Routing fix with hashes

--- OpenPlace/src/app/app.component.ts
+++ OpenPlace/src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, RouterLink],
   standalone: true,
 })
 export class AppComponent {
